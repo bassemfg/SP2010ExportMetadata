95a7d75f10567982bd1f64d94e903c485f10cba4
Bassem Yacoube <bassemfg@gmx.net>
2018-06-21 19:17:15 -0700

    2

--- SP2010ExportMetadata/SP2010ExportMetadata/Program.cs
+++ SP2010ExportMetadata/SP2010ExportMetadata/Program.cs
@@ -10,87 +10,96 @@ namespace SP2010ExportMetadata
     {
         static void Main(string[] args)
         {
-
-            string RootSiteCollection = System.Configuration.ConfigurationSettings.AppSettings["RootSiteCollection"];
-            //int i = 0;
-            int j = 0;
-            Microsoft.SharePoint.SPSite siteColl = new SPSite(RootSiteCollection);
-            StringBuilder sbFields = new StringBuilder();
-            StringBuilder sbVals = new StringBuilder();
-
-            StreamWriter sw = null;
-
-            foreach (SPWeb web in siteColl.AllWebs)
+            try
             {
-                Console.WriteLine("Processing site " + web.Url);
-                sbFields.Length = 0;
+                string RootSiteCollection = System.Configuration.ConfigurationSettings.AppSettings["RootSiteCollection"];
+                //int i = 0;
+                int j = 0;
+                Microsoft.SharePoint.SPSite siteColl = new SPSite(RootSiteCollection);
+                StringBuilder sbFields = new StringBuilder();
+                StringBuilder sbVals = new StringBuilder();
 
+                StreamWriter sw = null;
 
-                foreach (SPList list in web.Lists)
+                foreach (SPWeb web in siteColl.AllWebs)
                 {
-                    Console.WriteLine("Processing list " + list.ID);
-                    sbVals.Length = 0;
-                    if (list.BaseType == SPBaseType.DocumentLibrary && list.Hidden == false)
+                    Console.WriteLine("Processing site " + web.Url);
+                    sbFields.Length = 0;
+
+
+                    foreach (SPList list in web.Lists)
                     {
-                        j = 0;
-                        foreach (SPItem item in list.Items)
+                        Console.WriteLine("Processing list " + list.ID);
+                        sbVals.Length = 0;
+                        if (list.BaseType == SPBaseType.DocumentLibrary && list.Hidden == false)
                         {
-                            Console.WriteLine("Processing item " + item.ID);
-                            if (j == 0)
+                            j = 0;
+                            foreach (SPItem item in list.Items)
                             {
-                                sbFields.Append("SourcePath");
-                                sbFields.Append(',');
-                            }
-
-                            sbVals.Append(item["URL Path"].ToString());
-                            sbVals.Append(',');
-
-                            foreach (SPField field in item.Fields)
-                            {
-                                try
+                                Console.WriteLine("Processing item " + item.ID);
+                                if (j == 0)
                                 {
-                                    if (field.Hidden == false && field.Sealed == false)
-                                    {
-                                        if (j == 0)
-                                        {
-                                            sbFields.Append(field.ToString());
-                                            sbFields.Append(',');
-                                        }
-                                        if (item[field.ToString()] != null)
-                                            sbVals.Append(item[field.ToString()].ToString());
-                                        sbVals.Append(',');
-
-                                    }
+                                    sbFields.Append("SourcePath");
+                                    sbFields.Append(',');
                                 }
-                                catch { }
-                            }
-                            //remove last ','
-                            if (sbFields.Length > 0)
-                                sbFields.Length = sbFields.Length - 1;
-                            if (sbVals.Length > 0)
-                                sbVals.Length = sbVals.Length - 1;
-                            // add new lines
-                            sbFields.Append(@"
+
+                                sbVals.Append(item["URL Path"].ToString());
+                                sbVals.Append(',');
+
+                                foreach (SPField field in item.Fields)
+                                {
+                                    try
+                                    {
+                                        if (field.Hidden == false && field.Sealed == false)
+                                        {
+                                            if (j == 0)
+                                            {
+                                                sbFields.Append(field.ToString());
+                                                sbFields.Append(',');
+                                            }
+                                            if (item[field.ToString()] != null)
+                                                sbVals.Append(item[field.ToString()].ToString());
+                                            sbVals.Append(',');
+
+                                        }
+                                    }
+                                    catch { }
+                                }
+                                //remove last ','
+                                if (sbFields.Length > 0)
+                                    sbFields.Length = sbFields.Length - 1;
+                                if (sbVals.Length > 0)
+                                    sbVals.Length = sbVals.Length - 1;
+                                // add new lines
+                                sbFields.Append(@"
 "); sbVals.Append(@"
 ");
-                            j++;
+                                j++;
 
+                            }
                         }
-                    }
-                    sbFields.Append(sbVals.ToString());
-                    sbFields.Append(@"
+                        sbFields.Append(sbVals.ToString());
+                        sbFields.Append(@"
 ");
 
+                    }
+
+                    sw = new StreamWriter(@"c:\metadata_" + web.Url.Substring(web.Url.LastIndexOf(@"/") + 1) + @".csv");
+
+                    sw.Write(sbFields.ToString());
+                    sw.Write(sbVals.ToString());
+                    sw.Flush();
+                    sw.Close();
+
                 }
-
-                sw = new StreamWriter(@"c:\metadata_" + web.Url.Substring(web.Url.LastIndexOf(@"/") + 1) + @".csv");
-
-                sw.Write(sbFields.ToString());
-                sw.Write(sbVals.ToString());
-                sw.Flush();
-                sw.Close();
-
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.StackTrace);
+            }
+            
         }
     }
 }
